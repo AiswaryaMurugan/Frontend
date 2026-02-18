bf3f2b3e45f5d88d5407264ec132cf1619a84c9f
Aiswarya Murugan <aiswaryamurugan200@gmail.com>
2026-02-18 19:47:16 +0530

    Add files via upload

---
+++ Node-Project/script.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === "/" || req.url === "/home") {
+        fs.readFile("home.html", (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        });
+    }
+
+    else if (req.url === "/about") {
+        fs.readFile("about.html", (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        });
+    }
+
+    else if (req.url === "/contact") {
+        fs.readFile("contact.html", (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        });
+    }
+   else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>404 Page Not Found</h1>");
+    }
+
+});
+
+server.listen(4000, () => {
+    console.log("Server is running at http://localhost:4000");
+});
